288f27eaea15742d3c692f34fd848d3d4feca232
marczik <marek99928@wp.pl>
2022-09-26 11:41:17 +0200

    Create squares 16x16

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log("Hello");
+function createSquare() {
+	const rightPanel = document.querySelector(".right-panel");
+	for (let i = 0; i < 256; i++) {
+		const div = document.createElement("div");
+        div.classList.add('square');  
+		rightPanel.appendChild(div);
+	}
+}
+
+createSquare();
