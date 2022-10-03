95bd477143bfc27a8b849d81b74f017d619eb319
marczik <marek99928@wp.pl>
2022-10-03 09:43:04 +0200

    Add functions which create big, medium and small square. Change size in .right-panel

--- app.js
+++ app.js
@@ -1,10 +1,38 @@
-function createSquare() {
+function createBigSquare() {
 	const rightPanel = document.querySelector(".right-panel");
-	for (let i = 0; i < 256; i++) {
+	for (let i = 0; i < 192; i++) {
 		const div = document.createElement("div");
-        div.classList.add('square');  
+		div.classList.add("square");
+		div.classList.add("big-square");
 		rightPanel.appendChild(div);
 	}
 }
+//16 x 12
 
-createSquare();
+createBigSquare();
+
+function createMediumSquare() {
+	const rightPanel = document.querySelector(".right-panel");
+	for (let i = 0; i < 768; i++) {
+		const div = document.createElement("div");
+		div.classList.add("square");
+		div.classList.add("medium-square");
+		rightPanel.appendChild(div);
+	}
+}
+//32 x 24
+
+//createMediumSquare();
+
+function createSmallSquare() {
+	const rightPanel = document.querySelector(".right-panel");
+	for (let i = 0; i < 3072; i++) {
+		const div = document.createElement("div");
+		div.classList.add("square");
+		div.classList.add("small-square");
+		rightPanel.appendChild(div);
+	}
+}
+//64 x 48
+
+//createSmallSquare();
